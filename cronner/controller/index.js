6c9dbbaeaf54752bd2bcc03e44c69acedf07c1cf
Joskar <joskar.hernandez@tutenlabs.com>
2021-08-28 16:58:46 -0500

    feature: trabajos mandados a la cola

--- cronner/controller/index.js
+++ cronner/controller/index.js
@@ -3,10 +3,19 @@ const client = require('../bd');
 const getJobsForDate = async () => {
   const query = `SELECT j.id, j.scheduled_date, jt.title, j.script
     FROM job j INNER JOIN job_type jt ON j.job_type_id = jt.id
-    WHERE EXTRACT(EPOCH FROM (j.scheduled_date - now())) >= -60 AND EXTRACT(EPOCH FROM (j.scheduled_date - now())) < 0
+    WHERE EXTRACT(EPOCH FROM (j.scheduled_date - now())) >= -61 AND EXTRACT(EPOCH FROM (j.scheduled_date - now())) < 1
     AND j.state = 'SCHEDULED'`;
   const result = await client.query(query);
+  const result2 = await client.query('SELECT now()');
+  console.log(result2.rows);
   return result.rows;
 }
 
-module.exports = { getJobsForDate };
+const updateStateForJobs = async (jobs, state) => {
+  const jobsIds = jobs.map(job => job.id).join(', ');
+  const query = `UPDATE job SET state = $1 WHERE id IN (${jobsIds})`;
+  const result = await client.query(query, [state]);
+  return result;
+}
+
+module.exports = { getJobsForDate, updateStateForJobs };
--- cronner/crontasks.js
+++ cronner/crontasks.js
@@ -1,10 +1,21 @@
-const { getJobsForDate } = require('./controller');
+const { getJobsForDate, updateStateForJobs } = require('./controller');
 const { publisher } = require('./rabbit');
 
 const readEveryMinute = async (conn) => {
-  console.log('ASKING FOR JOBS');
   const jobs = await getJobsForDate();
   console.log(jobs);
+  if (jobs.length) {
+    const message = {
+      jobs,
+    };
+    publisher(conn, JSON.stringify(message), 'tasks', (err, message) => {
+      if (err) console.error(err);
+      if (message) console.error(err);
+      updateStateForJobs(jobs, 'SENT')
+    });
+  }
+
+
 }
 
 module.exports = { readEveryMinute };
